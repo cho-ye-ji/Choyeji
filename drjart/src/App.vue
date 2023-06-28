0e2ac8906a1f50e7075cb63f99e8022597febe5d
Choyeji <wldi1097@naver.com>
2023-06-28 16:44:59 +0900

    뷰 파일 생성 및 메인

---
+++ drjart/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </header>
+  <section class="sec1">
+    <div class="sec1_right">
+      <p> right </p>
+      <p> section </p>
+    </div>
+    <div class="sec1_left"></div>
+  </section>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MainSlide from './components/slide/MainSlide.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MainSlide
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ drjart/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+//Import css
+import "@/assets/css/common.css"
+
+createApp(App).mount('#app')
+
